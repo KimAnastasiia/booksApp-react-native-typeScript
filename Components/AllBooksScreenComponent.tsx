d6a7b192648a4a0f923e91b78f2e0fd3e41a8e2b
Anastasiia <anstasiakim2789@gmail.com>
2024-01-21 19:49:17 +0100

    show photo of book in allBooksList

--- Components/AllBooksScreenComponent.tsx
+++ Components/AllBooksScreenComponent.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, ScrollView, Image, TouchableOpacity, Alert, Pressable } from 'react-native';
 import { backendUrl, tokenFireBaseStorage } from '../Global';
 import styles from '../Utility/styles';
@@ -11,10 +11,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store"
 import { setBooks } from "../redux/booksReducer";
 
-type AllBooksScreenComponentProps=NativeStackScreenProps<RootStackParamList, 'AllBooks'>
+type AllBooksScreenComponentProps = NativeStackScreenProps<RootStackParamList, 'AllBooks'>
 
 const AllBooksScreenComponent: React.FC<AllBooksScreenComponentProps> = (props) => {
-  
+
   // Redux
   const dispatch = useDispatch();
   const books = useSelector((state: RootState) => state.books.books)
@@ -24,19 +24,26 @@ const AllBooksScreenComponent: React.FC<AllBooksScreenComponentProps> = (props) 
   }, [])
 
   let getAllBooks = async () => {
-   
+
     let response = await fetch(backendUrl + `/books`)
     if (response.ok) {
       let data = await response.json();
       //setBooks(data);
-      data.forEach(async(book:Book) => {
-        let response = await fetch(`https://firebasestorage.googleapis.com/v0/b/books-store-dc964.appspot.com/o/photos%2F${book.id}.png?alt=media&token=${tokenFireBaseStorage}`)
-        if(response.ok){
-          book.hasImg=true
-        }else{
-          book.hasImg=false
-        }
-      });
+      await Promise.all(
+        data.map(async (book: Book) => {
+
+          try {
+            let response = await fetch(`https://firebasestorage.googleapis.com/v0/b/books-store-dc964.appspot.com/o/photos%2F${book.id}.png?alt=media&token=${tokenFireBaseStorage}`)
+            if (response.ok) {
+              book.hasImg = true
+            } else {
+              book.hasImg = false
+            }
+          } catch (e) {
+            book.hasImg = false
+          }
+
+        }))
       dispatch(setBooks(data));
     }
   }
@@ -76,22 +83,31 @@ const AllBooksScreenComponent: React.FC<AllBooksScreenComponentProps> = (props) 
   };
   const BookList: React.FC<{ book: Book }> = ({ book }) => (
     <TouchableOpacity onPress={() => {
-      props.navigation.push('DetailsBook', { id: book.id } )
+      props.navigation.push('DetailsBook', { id: book.id })
     }}>
       <View style={styles.container}>
-        <View style={{ flexDirection: 'row', width:"84%"}}>
-          <View style={{ width: "20%"}}>
-            <Image
-              style={styles.tinyLogo}
-              source={book.hasImg? { uri: getBookImageUrl(book.id) } : defaultImageSource}
-            />
+        <View style={{ flexDirection: 'row', width: "84%" }}>
+          <View style={{ width: "20%" }}>
+
+            {book.hasImg &&
+              <Image
+                style={styles.tinyLogo}
+                source={{ uri: getBookImageUrl(book.id) }}
+              />
+            }
+            {!book.hasImg &&
+              <Image
+                style={styles.tinyLogo}
+                source={defaultImageSource}
+              />
+            }
           </View>
-          <View style={{ width: "80%"}}>
+          <View style={{ width: "80%" }}>
             <Text style={styles.title}>{book.title}</Text>
             <Text style={styles.author}>{book.author}</Text>
           </View>
         </View>
-        <View style={{ width: "16%"}}>
+        <View style={{ width: "16%" }}>
           <TouchableOpacity style={styles.deleteBookButton} onPress={() => { deleteBook(book.id) }} >
             <Image
               style={styles.binLogo}
@@ -101,11 +117,11 @@ const AllBooksScreenComponent: React.FC<AllBooksScreenComponentProps> = (props) 
         </View>
       </View>
     </TouchableOpacity>
-   
+
   )
 
   return (
-    <ScrollView style={{backgroundColor:"white"}}>
+    <ScrollView style={{ backgroundColor: "white" }}>
       {books.length > 0 ? (
         books.map((book) => <BookList key={book.id} book={book} />)
       ) : (
